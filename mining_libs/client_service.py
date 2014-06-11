892276910332697949dafbbd0afb8ab5dc89e178
Pau Escrich <p4u@dabax.net>
2014-06-11 12:28:53 +0200

    Remove unused control file functions

--- mining_libs/client_service.py
+++ mining_libs/client_service.py
@@ -16,11 +16,6 @@ class ClientMiningService(GenericEventHandler):
     timeout = None # Reference to IReactorTime object
     f = None # Factory of Stratum client
     ping_sent = False
-    cf_counter = 0
-    cf_path = None
-    cf_notif = 10
-    backup_active_interval_max = 20
-    backup_active_interval = backup_active_interval_max
     controlled_disconnect = False
     auth = False
     is_backup_active = False
@@ -41,63 +36,8 @@ class ClientMiningService(GenericEventHandler):
         return s
 
     @classmethod
-    def check_control_file(cls):
-        # Contorl file syntax is: <pool:port> [user:pass]
-        # Example: mypool.com:3333 user.1:foo
-
-        # If backup is active, wait for backup_active_interval_max to try again with original pool
-        if cls.is_backup_active:
-            cls.backup_active_interval -= 1
-            if cls.backup_active_interval < 1:
-                log.info("Backup pool is active, trying to connect back with the original pool")
-                cls.backup_active_interval = cls.backup_active_interval_max
-                cls.is_backup_active = False
-                # if cf_path is not enabled, sending reconnect. Pool host change is handled by the disconnect event
-                if cls.cf_path == None:
-                    cls.set_controlled_disconnect(False)
-                    hostport = list(cls.job_registry.f.main_host[::])
-                    cls.job_registry.f.reconnect(hostport[0], hostport[1], None)
-
-        if cls.cf_path != None and cls.cf_counter > cls.cf_notif and (not cls.is_backup_active):
-            cls.cf_counter = 0
-            log.info("Checking control file")
-            try:
-                with open(cls.cf_path,'r') as cf:
-                    data = cf.read()
-                    sdata = data.strip().split(' ')
-                    host = sdata[0].split(':')[0]
-                    port = int(sdata[0].split(':')[1])
-                    new_user_pass = False
-                    if len(sdata) > 1:
-                        user = sdata[1].split(':')[0]
-                        passw =  sdata[1].split(':')[1]
-                        if cls.auth:
-                            if user != cls.auth[0] or passw != cls.auth[1]:
-                                cls.auth = (user,passw)
-                                new_user_pass = True
-                        else:
-                            cls.auth = (user,passw)
-                            new_user_pass = True
-                new = list(cls.job_registry.f.main_host[::])
-                log.info("Current pool is %s:%d" % tuple(new))
-                if new_user_pass or new[0] != host or new[1] != port:
-                    new[0] = host
-                    new[1] = port
-                    log.info("Found new pool configuration on host control file, reconnecting to %s:%d" % tuple(new))
-                    log.info("New custom authorization data found %s:%s" %cls.auth)
-                    cls.set_controlled_disconnect(True)
-                    cls.job_registry.f.reconnect(new[0], new[1], None)
-
-            except:
-                log.error("Cannot open or read control file %s, keeping current pool configuration" % cls.cf_path)
-        elif cls.cf_path != None:
-            cls.cf_counter += 1
-
-    @classmethod
     def reset_timeout(cls):
         cls.last_notify_time = datetime.datetime.now()
-
-        cls.check_control_file()
         if cls.timeout != None:
             if not cls.timeout.called:
                 cls.timeout.cancel()
