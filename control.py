b413fa466347aaa866e19f6ff6ca90a648746002
Pau Escrich <p4u@dabax.net>
2014-06-16 13:20:50 +0200

    Extend control.py with help and port specification

--- control.py
+++ control.py
@@ -1,12 +1,17 @@
 import zmq
 import sys
+
+if len(sys.argv) < 3:
+    print("Usage: %s <control_port> <args...>")
+    sys.exit(1)
+
 context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://127.0.0.1:3999")
+socket = context.socket(zmq.REQ)    
+socket.connect("tcp://127.0.0.1:%s" %sys.argv[1])
 msg=""
-for a in sys.argv[1::]:
+for a in sys.argv[2::]:
 	msg += "%s " %a
 socket.send(msg)
 reply = socket.recv()
 print("> %s" %reply)
-
+context.destroy()
