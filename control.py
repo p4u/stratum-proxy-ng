ef6e01ac6e1b43dcb2a9e0bad0e135ed1c43979b
Pau Escrich <p4u@dabax.net>
2014-06-20 16:18:46 +0200

    Extend control.py to accept IP to manage

--- control.py
+++ control.py
@@ -6,12 +6,12 @@ def niceprint(data):
 	return json.dumps(data,sort_keys=True,indent=4, separators=(',', ': ')).__str__()
 
 if len(sys.argv) < 3:
-    print("Usage: %s <control_port> <query> [key1=value1 key2=value2]" %sys.argv[0])
+    print("Usage: %s <ip:control_port> <query> [key1=value1 key2=value2]" %sys.argv[0])
     sys.exit(1)
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)    
-socket.connect("tcp://127.0.0.1:%s" %sys.argv[1])
+socket.connect("tcp://%s" %sys.argv[1])
 msg = { 'query':sys.argv[2] }
 
 for a in sys.argv[3::]:
