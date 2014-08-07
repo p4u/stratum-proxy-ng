3a122cdfd27f0d335ab647d236a8917368d7a855
Pau Escrich <p4u@dabax.net>
2014-08-07 14:21:47 +0200

    Update configuration of setup.py

--- setup.py
+++ setup.py
@@ -14,16 +14,15 @@ from mining_libs import version
 args = {
     'name': 'stratum_mining_proxy',
     'version': version.VERSION,
-    'description': 'Getwork-compatible proxy for Stratum mining pools',
-    'author': 'slush',
-    'author_email': 'slush@satoshilabs.com',
+    'description': 'Stratum proxy new generation',
+    'author': 'slush/p4u',
+    'author_email': 'slush@satoshilabs.com/p4u@dabax.net',
     'url': 'http://mining.bitcoin.cz/stratum-mining/',
-    'py_modules': ['mining_libs.client_service', 'mining_libs.getwork_listener',
-                   'mining_libs.jobs', 'mining_libs.midstate',
-                   'mining_libs.multicast_responder', 'mining_libs.stratum_listener',
-                   'mining_libs.utils', 'mining_libs.version', 'mining_libs.worker_registry'],
+    'py_modules': ['mining_libs.client_service', 'mining_libs.share_stats',
+                   'mining_libs.jobs', 'mining_libs.stratum_listener',
+                   'mining_libs.utils', 'mining_libs.version'],
     'install_requires': ['setuptools>=0.6c11', 'twisted>=12.2.0', 'stratum>=0.2.15', 'argparse'],
-    'scripts': ['mining_proxy.py'],
+    'scripts': ['stproxy-ng.py'],
 }
 
 if py2exe != None:
